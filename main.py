8653e5210b2aa3368188c896bb549aa731a7bc99
John Stanwick <jwstanwick@gmail.com>
2023-09-06 14:20:32 -0400

    i hate comments

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from combine import create_new_shirt
 
 def main():
     create_new_shirt('https://www.geeksforgeeks.org/python-main-function/')
-  
-# Using the special variable 
-# __name__
+
 if __name__=="__main__":
     main()
